67821f298c636cf0684641f8666fa902bfc0f6f5
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2023-10-05 01:26:39 -0400

    implementing the authenticate method in the service

--- src/main/java/com/luisfelipemunoz/security/auth/AuthenticationService.java
+++ src/main/java/com/luisfelipemunoz/security/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.luisfelipemunoz.security.user.Role;
 import com.luisfelipemunoz.security.user.User;
 import com.luisfelipemunoz.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
                 .firstname(request.getFirstname())
@@ -31,6 +34,16 @@ public class AuthenticationService {
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();// todo handle the exception
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
