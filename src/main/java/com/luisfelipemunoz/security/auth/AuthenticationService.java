ae65720fd3aae22e10ddea98baf6a921cada6b9e
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2023-10-05 01:23:13 -0400

    implementing the register method in the service

---
+++ src/main/java/com/luisfelipemunoz/security/auth/AuthenticationService.java
@@ -0,0 +1,36 @@
+package com.luisfelipemunoz.security.auth;
+
+import com.luisfelipemunoz.security.config.JwtService;
+import com.luisfelipemunoz.security.user.Role;
+import com.luisfelipemunoz.security.user.User;
+import com.luisfelipemunoz.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
+}
