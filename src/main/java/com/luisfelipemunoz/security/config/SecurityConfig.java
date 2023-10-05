bc73a9f43d6e2ffd006c992870011f4a4c13e199
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2023-10-05 01:28:33 -0400

    allowing all the method within authController

--- src/main/java/com/luisfelipemunoz/security/config/SecurityConfig.java
+++ src/main/java/com/luisfelipemunoz/security/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig {
         httpSecurity.csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
