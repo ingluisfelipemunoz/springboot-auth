5b4e34e8847bd9475c3e350db31f76db5641a7cf
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2023-10-05 00:33:17 -0400

    adding methods check if the token is valid

--- src/main/java/com/luisfelipemunoz/security/config/JwtService.java
+++ src/main/java/com/luisfelipemunoz/security/config/JwtService.java
@@ -30,6 +30,19 @@ public class JwtService {
         return generateToken(new HashMap<>(), userDetails);
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return Jwts.builder()
                 .setClaims(extraClaims)
