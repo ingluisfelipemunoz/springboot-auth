6aa15430020c906b31178793288e3c53e40157be
ingluisfelipemunoz <ingluisfelipemunoz@gmail.com>
2023-10-04 22:23:44 -0400

    init

---
+++ src/main/java/com/luisfelipemunoz/security/config/JwtService.java
@@ -0,0 +1,10 @@
+package com.luisfelipemunoz.security.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String jwt) {
+        return null;
+    }
+}
---
+++ src/main/java/com/luisfelipemunoz/security/user/Role.java
@@ -0,0 +1,6 @@
+package com.luisfelipemunoz.security.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
